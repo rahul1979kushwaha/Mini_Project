b08b9b665b8f9edb830a739e55057cc606d26cf8
mr2space <princegoswami.space@gmail.com>
2023-11-26 16:10:20 +0530

    error fixing

--- Backend/UserAuth/urls.py
+++ Backend/UserAuth/urls.py
@@ -1,5 +1,5 @@
-from django.urls import URLPattern, path
+from django.urls import  path
 from . import views
 urlpatterns = [
-    path('', views.index, "login")
+    path('', views.index, name = "login")
 ]
