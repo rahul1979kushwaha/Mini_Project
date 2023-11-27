0c626961bd75f5cd9e32ac3a65ca4926391eb80a
mr2space <princegoswami.space@gmail.com>
2023-11-28 00:48:13 +0530

    sample

--- Backend/UserAuth/views.py
+++ Backend/UserAuth/views.py
@@ -2,5 +2,4 @@ from django.http import JsonResponse
 
 
 def index(request, *args, **kwargs):
-    m = 'abcdef'
-    return JsonResponse({'message':"hello this is msg", 'm':m})
+    return JsonResponse({'message':"hello this is msg"})
